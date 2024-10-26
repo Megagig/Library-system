6bc6ccf5a6b85665715e3bc328e38ab7f13362a2
Megagig <megagigsoftwaresolutions.com>
2024-10-26 23:10:55 +0100

    Get all books

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const createBook = require("../controllers/createBook");
-
-
-
-
-
-
+const createBook = require('../controllers/createBook');
+const getAllBooks = require('../controllers/getAllBooks');
 
 const router = express.Router();
 
-router.post("/add", createBook);
+router.post('/add', createBook);
+router.get('/all-books', getAllBooks);
 
 module.exports = router;
