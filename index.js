011dc77ddab90a3c83987077c12b79db88882a96
Megagig <megagigsoftwaresolutions.com>
2024-10-26 17:05:37 +0100

    Setup the server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+//setup simple route
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the application.' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
