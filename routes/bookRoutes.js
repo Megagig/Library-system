f7e8150f7c9f94b3667db198ba27a104a998aa2b
Megagig <megagigsoftwaresolutions.com>
2024-10-27 11:11:51 +0100

    Get Books By ID

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const createBook = require('../controllers/createBook');
 const getAllBooks = require('../controllers/getAllBooks');
+const getBookById = require('../controllers/getBookById');
 
 const router = express.Router();
 
 router.post('/add', createBook);
 router.get('/all-books', getAllBooks);
+router.get('/:id', getBookById);
 
 module.exports = router;
